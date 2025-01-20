54fbc87fdec8727f589f19130dd705532888cb52
Shuto Shikama <shuto.shikama@gmail.com>
2025-01-20 16:07:53 +0900

    [WIP] ShaderObject

---
+++ hello_shader_object/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let entry = ash::Entry::linked();
+
+    let instance = {
+        let extension_names = [
+            ash::ext::debug_utils::NAME.as_ptr(),
+            ash::khr::get_physical_device_properties2::NAME.as_ptr(),
+        ];
+        let layer_names = [c"VK_LAYER_KHRONOS_validation".as_ptr()];
+        let application_info =
+            ash::vk::ApplicationInfo::default().api_version(ash::vk::API_VERSION_1_3);
+        let create_info = ash::vk::InstanceCreateInfo::default()
+            .application_info(&application_info)
+            .enabled_extension_names(&extension_names)
+            .enabled_layer_names(&layer_names);
+        unsafe { entry.create_instance(&create_info, None) }
+    }
+    .unwrap();
+
+    let physical_device = unsafe { instance.enumerate_physical_devices() }.unwrap()[0];
+    let device = {
+        let mut features =
+            ash::vk::PhysicalDeviceShaderObjectFeaturesEXT::default().shader_object(true);
+        let enabled_extension_names = [ash::vk::EXT_SHADER_OBJECT_NAME.as_ptr()];
+        let properties = [1.0];
+        let queue_create_infos = [ash::vk::DeviceQueueCreateInfo::default()
+            .queue_family_index(0)
+            .queue_priorities(&properties)];
+        let create_info = ash::vk::DeviceCreateInfo::default()
+            .queue_create_infos(&queue_create_infos)
+            .enabled_extension_names(&enabled_extension_names)
+            .push_next(&mut features);
+        unsafe { instance.create_device(physical_device, &create_info, None) }
+    }
+    .unwrap();
+
+    // デバイス
+    unsafe { device.destroy_device(None) }
+
+    // インスタンス
+    unsafe { instance.destroy_instance(None) }
+}
