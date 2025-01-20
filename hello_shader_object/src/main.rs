0c64661f670496a2cabf00e270872fe308f38f9b
Shuto Shikama <@gmail.com>
2025-01-20 16:44:30 +0900

    fixup! [WIP] ShaderObject

--- hello_shader_object/src/main.rs
+++ hello_shader_object/src/main.rs
@@ -5,14 +5,21 @@ fn main() {
         let extension_names = [
             ash::ext::debug_utils::NAME.as_ptr(),
             ash::khr::get_physical_device_properties2::NAME.as_ptr(),
+            ash::khr::portability_enumeration::NAME.as_ptr(),
         ];
+        let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+            ash::vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+        } else {
+            ash::vk::InstanceCreateFlags::default()
+        };
         let layer_names = [c"VK_LAYER_KHRONOS_validation".as_ptr()];
         let application_info =
             ash::vk::ApplicationInfo::default().api_version(ash::vk::API_VERSION_1_3);
         let create_info = ash::vk::InstanceCreateInfo::default()
             .application_info(&application_info)
             .enabled_extension_names(&extension_names)
-            .enabled_layer_names(&layer_names);
+            .enabled_layer_names(&layer_names)
+            .flags(create_flags);
         unsafe { entry.create_instance(&create_info, None) }
     }
     .unwrap();
@@ -20,6 +27,7 @@ fn main() {
     let physical_device = unsafe { instance.enumerate_physical_devices() }.unwrap()[0];
     let extensions =
         unsafe { instance.enumerate_device_extension_properties(physical_device) }.unwrap();
+
     let is_contains = extensions.iter().any(|x| {
         let Ok(name) = x.extension_name_as_c_str() else {
             return false;
@@ -40,6 +48,7 @@ fn main() {
         let mut features =
             ash::vk::PhysicalDeviceShaderObjectFeaturesEXT::default().shader_object(true);
         let enabled_extension_names = [ash::vk::EXT_SHADER_OBJECT_NAME.as_ptr()];
+        // let enabled_extension_names = [];
         let properties = [1.0];
         let queue_create_infos = [ash::vk::DeviceQueueCreateInfo::default()
             .queue_family_index(0)
