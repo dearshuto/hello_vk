9c39403f4ca6a696daa047699adaf872b1cf7a08
Shuto Shikama <@gmail.com>
2025-01-24 14:20:55 +0900

    fixup! [WIP] ShaderObject

--- hello_shader_object/src/main.rs
+++ hello_shader_object/src/main.rs
@@ -2,8 +2,6 @@ use std::borrow::Cow;
 
 use winit::{
     application::ApplicationHandler,
-    event::StartCause,
-    event_loop::ActiveEventLoop,
     raw_window_handle::{HasDisplayHandle, HasWindowHandle},
 };
 
@@ -16,17 +14,20 @@ fn main() {
 
 struct Instance {
     instance: ash::Instance,
+    debug_utils_instance: ash::ext::debug_utils::Instance,
+    debug_utils_messanger: ash::vk::DebugUtilsMessengerEXT,
     device: ash::Device,
     queue: ash::vk::Queue,
-    // surface_instance: ash::khr::surface::Instance,
-    // surface: ash::vk::SurfaceKHR,
-    // swapchain: ash::vk::SwapchainKHR,
-    // swapchain_loader: ash::khr::swapchain::Device,
-    // swapchain_image_view: [ash::vk::ImageView; 2],
-    // command_pool: ash::vk::CommandPool,
-    // command_buffer: ash::vk::CommandBuffer,
-    // acquire_next_image_semaphore: ash::vk::Semaphore,
-    // command_semaphore: ash::vk::Semaphore,
+    surface_instance: ash::khr::surface::Instance,
+    surface: ash::vk::SurfaceKHR,
+    swapchain: ash::vk::SwapchainKHR,
+    swapchain_loader: ash::khr::swapchain::Device,
+    swapchain_image_view: [ash::vk::ImageView; 2],
+    command_pool: ash::vk::CommandPool,
+    command_buffer: ash::vk::CommandBuffer,
+    acquire_next_image_semaphore: ash::vk::Semaphore,
+    command_semaphore: ash::vk::Semaphore,
+    render_pass: ash::vk::RenderPass,
     // shader_object_device: ash::ext::shader_object::Device,
     // shader_objects: [ash::vk::ShaderEXT; 2],
 }
@@ -48,6 +49,142 @@ impl App {
             entry,
             instance: None,
         }
+    }
+
+    pub fn request_redraw(&mut self) {
+        let Some(instance) = &self.instance else {
+            return;
+        };
+
+        let device = &instance.device;
+        let queue = instance.queue;
+        let swapchain_loader = &instance.swapchain_loader;
+        let swapchain = instance.swapchain;
+        let swapchain_images = unsafe { swapchain_loader.get_swapchain_images(swapchain) }.unwrap();
+        let present_image_views = &instance.swapchain_image_view;
+        let command_buffer = instance.command_buffer;
+        let acquire_next_image_semaphore = instance.acquire_next_image_semaphore;
+        let render_command_semaphore = instance.command_semaphore;
+
+        unsafe { device.queue_wait_idle(queue) }.unwrap();
+
+        let (frame_index, _) = unsafe {
+            swapchain_loader.acquire_next_image(
+                swapchain,
+                u64::MAX,
+                acquire_next_image_semaphore,
+                ash::vk::Fence::null(),
+            )
+        }
+        .unwrap();
+
+        let command_buffer_begin_info = ash::vk::CommandBufferBeginInfo::default()
+            .flags(ash::vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT);
+        unsafe { device.begin_command_buffer(command_buffer, &command_buffer_begin_info) }.unwrap();
+
+        unsafe {
+            device.cmd_pipeline_barrier(
+                command_buffer,
+                ash::vk::PipelineStageFlags::TOP_OF_PIPE,
+                ash::vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+                ash::vk::DependencyFlags::empty(),
+                &[], // MemoryBariier
+                &[], // BufferMemoryBariier
+                &[ash::vk::ImageMemoryBarrier::default()
+                    .image(swapchain_images[frame_index as usize])
+                    .src_access_mask(ash::vk::AccessFlags::empty())
+                    .dst_access_mask(ash::vk::AccessFlags::COLOR_ATTACHMENT_WRITE)
+                    .old_layout(ash::vk::ImageLayout::UNDEFINED)
+                    .new_layout(ash::vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                    .subresource_range(
+                        ash::vk::ImageSubresourceRange::default()
+                            .aspect_mask(ash::vk::ImageAspectFlags::COLOR)
+                            .base_mip_level(0)
+                            .level_count(1)
+                            .base_array_layer(0)
+                            .layer_count(1),
+                    )],
+            )
+        }
+
+        unsafe {
+            device.cmd_begin_rendering(
+                command_buffer,
+                &ash::vk::RenderingInfo::default()
+                    .render_area(
+                        ash::vk::Rect2D::default()
+                            .extent(ash::vk::Extent2D::default().width(640).height(480)),
+                    )
+                    .layer_count(1)
+                    .color_attachments(&[ash::vk::RenderingAttachmentInfo::default()
+                        .load_op(ash::vk::AttachmentLoadOp::CLEAR)
+                        .store_op(ash::vk::AttachmentStoreOp::STORE)
+                        .image_layout(ash::vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                        .clear_value(ash::vk::ClearValue {
+                            color: ash::vk::ClearColorValue {
+                                float32: [1.0, 1.0, 1.0, 1.0],
+                            },
+                        })
+                        .image_view(present_image_views[frame_index as usize])]),
+            );
+        }
+
+        unsafe { device.cmd_end_rendering(command_buffer) };
+
+        unsafe {
+            device.cmd_pipeline_barrier(
+                command_buffer,
+                ash::vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+                ash::vk::PipelineStageFlags::BOTTOM_OF_PIPE,
+                ash::vk::DependencyFlags::empty(),
+                &[], // MemoryBariier
+                &[], // BufferMemoryBariier
+                &[ash::vk::ImageMemoryBarrier::default()
+                    .image(swapchain_images[frame_index as usize])
+                    .src_access_mask(ash::vk::AccessFlags::COLOR_ATTACHMENT_WRITE)
+                    .dst_access_mask(ash::vk::AccessFlags::empty())
+                    .old_layout(ash::vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                    .new_layout(ash::vk::ImageLayout::PRESENT_SRC_KHR)
+                    .subresource_range(
+                        ash::vk::ImageSubresourceRange::default()
+                            .aspect_mask(ash::vk::ImageAspectFlags::COLOR)
+                            .base_mip_level(0)
+                            .level_count(1)
+                            .base_array_layer(0)
+                            .layer_count(1),
+                    )],
+            )
+        }
+
+        unsafe { device.end_command_buffer(command_buffer) }.unwrap();
+
+        // コマンドの提出
+        let wait_semaphores = [acquire_next_image_semaphore];
+        let signal_semaphores = [render_command_semaphore];
+        let command_buffers = [command_buffer];
+        let wait_mask = [ash::vk::PipelineStageFlags::BOTTOM_OF_PIPE
+            | ash::vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT];
+        let submit_info = ash::vk::SubmitInfo::default()
+            .wait_semaphores(&wait_semaphores)
+            .signal_semaphores(&signal_semaphores)
+            .wait_dst_stage_mask(&wait_mask)
+            .command_buffers(&command_buffers);
+        unsafe { device.queue_submit(queue, &[submit_info], ash::vk::Fence::null()) }.unwrap();
+
+        // 表示
+        let wait_semaphores = [render_command_semaphore];
+        let swapchains = [swapchain];
+        let indices = [frame_index];
+        unsafe {
+            swapchain_loader.queue_present(
+                queue,
+                &ash::vk::PresentInfoKHR::default()
+                    .wait_semaphores(&wait_semaphores)
+                    .swapchains(&swapchains)
+                    .image_indices(&indices),
+            )
+        }
+        .unwrap();
     }
 }
 
@@ -85,7 +222,7 @@ impl ApplicationHandler for App {
                 c"VK_LAYER_KHRONOS_shader_object".as_ptr(),
             ];
             let application_info =
-                ash::vk::ApplicationInfo::default().api_version(ash::vk::API_VERSION_1_2);
+                ash::vk::ApplicationInfo::default().api_version(ash::vk::API_VERSION_1_3);
             let create_info = ash::vk::InstanceCreateInfo::default()
                 .application_info(&application_info)
                 .enabled_extension_names(&extension_names)
@@ -111,7 +248,7 @@ impl ApplicationHandler for App {
         let debug_utils =
             unsafe { debug_utils_loader.create_debug_utils_messenger(&debug_info, None) }.unwrap();
 
-        let surface_loader = ash::khr::surface::Instance::new(&self.entry, &instance);
+        let surface_instance = ash::khr::surface::Instance::new(&self.entry, &instance);
         let surface = unsafe {
             ash_window::create_surface(
                 &self.entry,
@@ -139,7 +276,7 @@ impl ApplicationHandler for App {
                         }
 
                         if !unsafe {
-                            surface_loader.get_physical_device_surface_support(
+                            surface_instance.get_physical_device_surface_support(
                                 *physical_device,
                                 index as u32,
                                 surface,
@@ -174,8 +311,10 @@ impl ApplicationHandler for App {
         }
 
         let device = {
-            let mut features =
+            let mut shader_object_features =
                 ash::vk::PhysicalDeviceShaderObjectFeaturesEXT::default().shader_object(true);
+            let mut dynamic_rendering_features =
+                ash::vk::PhysicalDeviceDynamicRenderingFeatures::default().dynamic_rendering(true);
             let enabled_extension_names = [
                 ash::khr::swapchain::NAME.as_ptr(),
                 ash::vk::EXT_SHADER_OBJECT_NAME.as_ptr(),
@@ -191,19 +330,21 @@ impl ApplicationHandler for App {
             let create_info = ash::vk::DeviceCreateInfo::default()
                 .queue_create_infos(&queue_create_infos)
                 .enabled_extension_names(&enabled_extension_names)
-                .push_next(&mut features);
+                .push_next(&mut shader_object_features)
+                .push_next(&mut dynamic_rendering_features);
             unsafe { instance.create_device(physical_device, &create_info, None) }
         }
         .unwrap();
 
         let queue = unsafe { device.get_device_queue(queue_family_index as u32, 0) };
 
-        let surface_format =
-            unsafe { surface_loader.get_physical_device_surface_formats(physical_device, surface) }
-                .unwrap()[0];
+        let surface_format = unsafe {
+            surface_instance.get_physical_device_surface_formats(physical_device, surface)
+        }
+        .unwrap()[0];
 
         let surface_capabilities = unsafe {
-            surface_loader.get_physical_device_surface_capabilities(physical_device, surface)
+            surface_instance.get_physical_device_surface_capabilities(physical_device, surface)
         }
         .unwrap();
 
@@ -269,13 +410,34 @@ impl ApplicationHandler for App {
         };
 
         // コマンドバッファー
-        let command_buffers = {
+        let command_buffer = {
             let command_buffer_allocate_info = ash::vk::CommandBufferAllocateInfo::default()
-                .command_buffer_count(2)
+                .command_buffer_count(1)
                 .command_pool(command_pool)
                 .level(ash::vk::CommandBufferLevel::PRIMARY);
-            unsafe { device.allocate_command_buffers(&command_buffer_allocate_info) }.unwrap()
+            unsafe { device.allocate_command_buffers(&command_buffer_allocate_info) }.unwrap()[0]
         };
+
+        let render_pass = {
+            let attatchments = [ash::vk::AttachmentDescription::default()
+                .format(surface_format.format)
+                .initial_layout(ash::vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                .final_layout(ash::vk::ImageLayout::PRESENT_SRC_KHR)
+                .load_op(ash::vk::AttachmentLoadOp::CLEAR)
+                .store_op(ash::vk::AttachmentStoreOp::STORE)
+                .samples(ash::vk::SampleCountFlags::TYPE_1)];
+            let subpasses = [ash::vk::SubpassDescription::default()
+                .pipeline_bind_point(ash::vk::PipelineBindPoint::GRAPHICS)
+                .color_attachments(&[ash::vk::AttachmentReference {
+                    attachment: 0,
+                    layout: ash::vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL,
+                }])];
+            let create_info = ash::vk::RenderPassCreateInfo::default()
+                .attachments(&attatchments)
+                .subpasses(&subpasses);
+            unsafe { device.create_render_pass(&create_info, None) }.unwrap()
+        };
+
         // let shader_object_device = ash::ext::shader_object::Device::new(&instance, &device);
 
         // let shader_create_info = [
@@ -286,6 +448,32 @@ impl ApplicationHandler for App {
         //         .create_shaders(&shader_create_info, None)
         //         .unwrap()
         // };
+
+        let (acquire_next_image_semaphore, command_semaphore) = {
+            let create_info = ash::vk::SemaphoreCreateInfo::default();
+            let acquire_next_image_semaphore =
+                unsafe { device.create_semaphore(&create_info, None) }.unwrap();
+            let command_semaphore = unsafe { device.create_semaphore(&create_info, None) }.unwrap();
+            (acquire_next_image_semaphore, command_semaphore)
+        };
+
+        self.instance = Some(Instance {
+            instance,
+            debug_utils_instance: debug_utils_loader,
+            debug_utils_messanger: debug_utils,
+            device,
+            queue,
+            surface_instance,
+            surface,
+            swapchain,
+            swapchain_loader,
+            swapchain_image_view: [present_image_view[0], present_image_view[1]],
+            command_pool,
+            command_buffer,
+            render_pass,
+            acquire_next_image_semaphore,
+            command_semaphore,
+        });
     }
 
     fn window_event(
@@ -294,11 +482,89 @@ impl ApplicationHandler for App {
         _window_id: winit::window::WindowId,
         event: winit::event::WindowEvent,
     ) {
+        if let Some(window) = &self.window {
+            window.request_redraw();
+        }
+
         match event {
             winit::event::WindowEvent::Resized(_) => {}
+            winit::event::WindowEvent::RedrawRequested => self.request_redraw(),
             winit::event::WindowEvent::CloseRequested => event_loop.exit(),
             _ => {}
         }
+    }
+}
+
+impl Drop for App {
+    fn drop(&mut self) {
+        let Some(instance) = &self.instance else {
+            return;
+        };
+
+        // GPU の処理待ち
+        unsafe { instance.device.queue_wait_idle(instance.queue) }.unwrap();
+
+        unsafe {
+            instance
+                .device
+                .destroy_semaphore(instance.command_semaphore, None);
+            instance
+                .device
+                .destroy_semaphore(instance.acquire_next_image_semaphore, None);
+        }
+
+        // レンダーパス
+        unsafe {
+            instance
+                .device
+                .destroy_render_pass(instance.render_pass, None)
+        }
+
+        // コマンドバッファー
+        unsafe {
+            instance
+                .device
+                .free_command_buffers(instance.command_pool, &[instance.command_buffer]);
+        }
+
+        // コマンドプール
+        unsafe {
+            instance
+                .device
+                .destroy_command_pool(instance.command_pool, None)
+        }
+
+        // イメージ
+        for image in instance.swapchain_image_view {
+            unsafe { instance.device.destroy_image_view(image, None) }
+        }
+
+        // スワップチェーン
+        unsafe {
+            instance
+                .swapchain_loader
+                .destroy_swapchain(instance.swapchain, None)
+        }
+
+        // サーフェイス
+        unsafe {
+            instance
+                .surface_instance
+                .destroy_surface(instance.surface, None)
+        }
+
+        // デバッグ情報
+        unsafe {
+            instance
+                .debug_utils_instance
+                .destroy_debug_utils_messenger(instance.debug_utils_messanger, None)
+        }
+
+        // デバイス
+        unsafe { instance.device.destroy_device(None) }
+
+        // インスタンス
+        unsafe { instance.instance.destroy_instance(None) }
     }
 }
 
